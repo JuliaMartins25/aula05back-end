9db0ff8d055628067e20b407cc0c8038753a0411
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-08-29 11:50:28 -0300

    refactor: start to refactor api routes

---
+++ src/routes/personagens.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+const personagensRoutes = Router()
+const personagens= [
+    {
+        id: 100,
+        nome: "eleven",
+        studio: "netflix",
+        vivo: true
+    },
+
+    {
+        id: 100,
+        nome: "mike",
+        studio: "",
+        vivo: true
+    },
+
+    {
+        id: 100,
+        nome: "max",
+        studio: "netflix",
+        vivo: true
+    },
+    
+]
