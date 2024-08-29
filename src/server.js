80c1c9cdd0821515b8e3effe76e67a12523fc0f5
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-08-29 10:50:25 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,12 +8,63 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 2,
+        nome: "Alegria",
+        cor: "Amarelo"
+    },
+
+    {
+        id: 3,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+
+]
+
+const personagens= [
+    {
+        id: 100,
+        nome: "eleven",
+        studio: "netflix",
+        vivo: true
+    },
+
+    {
+        id: 100,
+        nome: "mike",
+        studio: "",
+        vivo: true
+    },
+
+    {
+        id: 100,
+        nome: "max",
+        studio: "netflix",
+        vivo: true
+    },
+    
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 })
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
+})
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+})
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
 })
 
 app.listen(serverPort, () => {
